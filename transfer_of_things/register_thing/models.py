f0a661451ba16a8e73d259090986d35f59f6f944
Crazyb3av3r <maxymviunov@gmail.com>
2022-10-29 20:28:18 +0200

    create_institution_model

--- transfer_of_things/register_thing/models.py
+++ transfer_of_things/register_thing/models.py
@@ -1,7 +1,20 @@
 from django.db import models
 
 # Create your models here.
+TYPE_CHOICES = (
+    (1, 'fundacja'),
+    (2, 'organizacja pozarządowa'),
+    (3, 'zbiórka lokalna')
+)
 
 
 class Category(models.Model):
     name = models.CharField(max_length=64)
+
+
+class Institution(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    type = models.IntegerField(choices=TYPE_CHOICES, default=1)
+    categories = models.ManyToManyField(Category, related_name='category')
+    
